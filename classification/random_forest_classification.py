60518da9aff0090a994ebe8071dcf2cba92282a5
Alejandro Jiménez-González <alejimgon@gmail.com>
2025-02-18 14:04:54 +0100

    Add folder tree and update scripts

--- classification/random_forest_classification.py
+++ classification/random_forest_classification.py
@@ -7,16 +7,13 @@
 import numpy as np # Allows us to work with arrays
 import matplotlib.pyplot as plt # Allows us to plot charts
 import pandas as pd # Allows us to import datasets and create the matrix of features and dependent variable
-import os # Allows us to interact with the operating system
 
-# Get the directory of the current script.
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Change the working directory to the script's directory.
-os.chdir(script_dir)
+# Setting the path to the data folder
+main_repo_folder = '/'.join(__file__.split('/')[:-1])
+data_folder = f'{main_repo_folder}/data'
 
 # Importing the dataset
-dataset = pd.read_csv('ENTER_THE_NAME_OF_YOUR_DATASET_HERE.csv')
+dataset = pd.read_csv(f'{data_folder}/YOUR_DATASET_.csv')
 X = dataset.iloc[:, :-1].values # ilock stands for locate indexes. [rows, columns] : means all the rows and :-1 all the columns except the last one.
 y = dataset.iloc[:, -1].values # : means all the rows and -1
 
